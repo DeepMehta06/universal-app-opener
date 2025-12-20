609efeb732e878cba6702f408de663974e4fc5b5
yash-pouranik <yashpouranik124@gmail.com>
2025-12-20 16:52:53 +0530

    misc: add comment explaining null value

--- packages/core/src/platforms/instagram.ts
+++ packages/core/src/platforms/instagram.ts
@@ -8,6 +8,7 @@ export const instagramHandler: DeepLinkHandler = {
     const id = match[2];
 
     const deepLinks: Record<string, { ios: string | null; android: string }> = {
+      // ios to null? : to trigger Universal Link fallback, as custom scheme requires a numeric ID.
       p: {
         ios: null,
         android: `intent://instagram.com/p/${id}#Intent;package=com.instagram.android;scheme=https;end`,
@@ -20,7 +21,7 @@ export const instagramHandler: DeepLinkHandler = {
         ios: null,
         android: `intent://instagram.com/tv/${id}#Intent;package=com.instagram.android;scheme=https;end`,
       },
-      // 'default' represents the user profile
+      // default represents the user profile
       default: {
         ios: `instagram://user?username=${id}`,
         android: `intent://user?username=${id}#Intent;scheme=instagram;package=com.instagram.android;end`,
