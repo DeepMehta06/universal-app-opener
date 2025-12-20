9cff6582764fea8dfd7b110eba44e1c25b654330
yash-pouranik <yashpouranik124@gmail.com>
2025-12-20 15:45:03 +0530

    feat: add support for instagram posts and reels deep links

--- packages/core/src/platforms/instagram.ts
+++ packages/core/src/platforms/instagram.ts
@@ -2,16 +2,37 @@ import { DeepLinkHandler } from '../types';
 
 export const instagramHandler: DeepLinkHandler = {
   match: (url) =>
-    url.match(/instagram\.com\/([^/?]+)/),
+    url.match(/instagram\.com\/(?:(p|reel|tv)\/)?([^/?]+)/),
 
   build: (webUrl, match) => {
-    const username = match[1];
+    const typeOfUrl = match[1];
 
-    return {
-      webUrl,
-      ios: `instagram://user?username=${username}`,
-      android: `intent://user?username=${username}#Intent;scheme=instagram;package=com.instagram.android;end`,
-      platform: 'instagram',
-    };
+    if(typeOfUrl === 'p'){
+      const postId = match[2];
+      return {
+        webUrl,
+        ios: null,
+        android: `intent://instagram.com/p/${postId}#Intent;package=com.instagram.android;scheme=https;end`,
+        platform: 'instagram',
+      };
+    }
+    else if(typeOfUrl === 'reel'){
+      const reelId = match[2];
+      return {
+        webUrl,
+        ios: null,
+        android: `intent://instagram.com/reel/${reelId}#Intent;package=com.instagram.android;scheme=https;end`,
+        platform: 'instagram',
+      };
+    }
+    else{
+      const user = match[2];
+      return {
+        webUrl,
+        ios: `instagram://user?username=${user}`,
+        android: `intent://user?username=${user}#Intent;scheme=instagram;package=com.instagram.android;end`,
+        platform: 'instagram',
+      };
+    }
   },
 };
